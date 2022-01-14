156dce1d3bca20878a862d00b8206a7792f9636b
germanrcb1105 <germanrcb@hotmail.com>
2022-01-14 01:28:59 -0500

    Se Crea Pagina de Incio con Validacion de Inicio de Sesion

---
+++ SHEL/Controllers/AccesoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SHEL.Models;
+
+namespace SHEL.Controllers
+{
+    public class AccesoController : Controller
+    {
+ 
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Persona(string identificacion, string password)
+        {
+            try
+            {
+                using (SIHEntities db = new SIHEntities())
+                {
+                    var lst = from d in db.mPersona
+                              where d.identificacion == identificacion && d.clave == password && d.estado == true
+                              select d;
+                    if (lst.Count() > 0)
+                    {
+                        mPersona oUser = lst.First();
+                        Session["identificacion"] = oUser;
+                        return Content("1");
+                    }
+                    else
+                    {
+                        return Content("Usuario o Clave invalido");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Content("Ocurrio un Error" + ex.Message);
+            }
+
+
+        }
+        public ActionResult Cerrar()
+        {
+            Session["identificacion"] = null;
+            return View();
+        }
+    }
+}
---
+++ SHEL/Filtros/clsSesiones.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SHEL.Controllers;
+using SHEL.Models;
+
+namespace SHEL.Filtros
+{
+        public class clsSesiones : ActionFilterAttribute
+        {
+            public override void OnActionExecuting(ActionExecutingContext filterContext)
+            {
+                if (HttpContext.Current.Request.RawUrl.ToString() == "/Registro/Persona")
+                {
+                    //filterContext.HttpContext.Response.Redirect("~/Registro/pCrear");
+                }
+                else
+                {
+
+                    var oUser = (mPersona)HttpContext.Current.Session["identificacion"];
+
+                    if (oUser == null)
+                    {
+                        if (filterContext.Controller is AccesoController == false)
+                        {
+                            filterContext.HttpContext.Response.Redirect("/Acceso/Index");
+                        }
+                    }
+                    else
+                    {
+                        if (filterContext.Controller is AccesoController == true)
+                        {
+                            filterContext.HttpContext.Response.Redirect("~/Home/Index");
+                        }
+                    }
+                }
+                base.OnActionExecuting(filterContext);
+            }
+        }
+    }
+    /*if (HttpContext.Current.Request.RawUrl.ToString() == "/Registro")
+                    {
+                        filterContext.HttpContext.Response.Redirect("~/Registro/Buscar");
+                    }
+                    else
+                    { }*/
+
+    /*else
+    {
+        if (HttpContext.Current.Request.RawUrl.ToString() == "/Acceso/Registro")
+        {
+            filterContext.HttpContext.Response.Redirect("~/Acceso/Registro");
+        }
+        else
+        {
+            filterContext.HttpContext.Response.Redirect("~/Home/Index");
+        }
+    }*/
