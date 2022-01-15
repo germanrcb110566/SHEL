aa292caf62a8529c26a0f0c068ee670e44a342f9
germanrcb1105 <germanrcb@hotmail.com>
2022-01-15 14:36:08 -0500

    Se crea la consulta de parametros del sistema

---
+++ SHEL/Controllers/ParametrosController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using SHEL.Models.ViewModel;
+using SHEL.Models;
+
+namespace SHEL.Controllers
+{
+    public class ParametrosController : Controller
+    {
+        // GET: Parametros
+        public ActionResult Index()
+        {
+            List<ParametrosViewModel> lst = null;
+            using (SHELEntities db = new SHELEntities())
+            {
+                lst = (from d in db.mParametros
+                       select new ParametrosViewModel
+                       {
+                           smtpserver = d.smtpserver,
+                           smtppuerto = d.smtppuerto,
+                           correo_sistema = d.correo_sistema
+                       }).ToList();
+            
+            }
+            return View(lst);
+        }
+
+
+        [HttpGet]
+        public ActionResult Grabar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Grabar(ParametrosViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (var db = new SHEL.Models.SHELEntities())
+            {
+                mParametros oParametros = new mParametros();
+                oParametros.smtpserver = model.smtpserver;
+                oParametros.smtppuerto = model.smtppuerto;
+                oParametros.correo_sistema = model.correo_sistema;
+                oParametros.clave_correo = model.clave_correo;
+
+                db.mParametros.Add(oParametros);
+                db.SaveChanges();
+
+            }
+            return Redirect(Url.Content("~/Parametros"));
+        }
+    }
+}
---
+++ SHEL/Models/ViewModel/ParametrosViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace SHEL.Models.ViewModel
+{
+    public class ParametrosViewModel
+    {
+        [Required]
+        public string smtpserver { set; get; }
+        [Required] 
+        public int  smtppuerto { set; get; }
+        [Required] 
+        [EmailAddress]
+        [Display(Name ="Correo Electónico")]
+        public string  correo_sistema { set; get; }
+        [Required]
+        [StringLength(5,ErrorMessage ="El {0} debe tener al menos {1} caracteres",MinimumLength =4)]
+        [Display(Name = "Contraseña")]
+        [DataType(DataType.Password)]
+        public string  clave_correo { set; get; }
+        [Display(Name = "Confirmar Contraseña")]
+        [DataType(DataType.Password)]
+        [Compare("",ErrorMessage ="Las contraseñas no son iguales")]
+        public string  confirmar_clave_correo { set; get; }
+    }
+}
