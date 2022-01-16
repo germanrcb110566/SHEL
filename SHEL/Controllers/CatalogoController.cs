500dcccae7238a6a2e19970994aa8fd37544fb7e
germanrcb1105 <germanrcb@hotmail.com>
2022-01-16 02:02:20 -0500

    Creacion de CRUD de Catalogos

---
+++ SHEL/Controllers/CatalogoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SHEL.Controllers
+{
+    public class CatalogoController : Controller
+    {
+        // GET: Catalogo
+        public ActionResult Index()
+        {
+            List<SelectListItem> lst = new List<SelectListItem>();
+            using (Models.SHELEntities db = new Models.SHELEntities())
+            {
+                lst = (from d in db.mCatalogo                
+                       select new SelectListItem
+                       {
+                           Value = d.registro_id.ToString(),
+                           Text = d.catalogo
+                       }).ToList();
+
+            }
+                return View(lst);
+        }
+    }
+}
